d643beffeb99c1cff816687784ac8f95373ee785
Komron <komronabdujabbor@gmail.com>
2023-06-25 18:39:15 +0500

    joriy turgan faylda ishlaydi, va textni parametr sifatida kiritish mumkin

--- Working with files/Program.cs
+++ Working with files/Program.cs
@@ -1,40 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Dynamic;
 using static System.Net.Mime.MediaTypeNames;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Good Job");
 
-FileService.DirCreate("papka");
-FileService.WriteFile("papka", "Hello world");
+FileService.DirCreateagain();
+FileService.WriteFile("Good job");
 
 public static class FileService
 {
+    static string dirname = "Papka";
     static FileService()
     {
     }
-    public static string file = "file";
-    public static void DirCreate(string dirname)
-    {
-        if (!Directory.Exists(@$"D:\{dirname}"))
-        {
-            Directory.CreateDirectory(@$"D:\{dirname}");
-        }
+    //public static string file = "file";
+    //public static void DirCreate()
+    //{
+    //    if (!Directory.Exists(@$"D:\{dirname}"))
+    //    {
+    //        Directory.CreateDirectory(@$"D:\{dirname}");
+    //    }
         
-        for (int i = 0; i < 10; i++)
-        {
-            string path = Path.Combine(@$"D:\{dirname}", file+$"{i}"+".txt");
-            Console.WriteLine(path);
-            using (FileStream fs = File.Create(path))
-            { }
-        }
-    }
-    public static void WriteFile(string dirname, string text)
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        string path = Path.Combine(@$"D:\{dirname}", $"{i}.txt");
+    //        Console.WriteLine(path);
+    //        using (FileStream fs = File.Create(path))
+    //        { }
+    //    }
+    //}
+    public static void WriteFile(string text)
     {
-        foreach (string file in Directory.GetFiles(@$"D:\{dirname}"))
+
+        string yol = Directory.GetCurrentDirectory();
+        yol=Path.Combine(yol, dirname);
+        foreach (string file in Directory.GetFiles(yol))
         {
             using (StreamWriter writer = new StreamWriter(file, true))
             {
                 writer.WriteLine(text);
             }
+        }
+    }
+    public static void DirCreateagain()
+    {
+        string newpath = "";
+        string path = $"{Directory.GetCurrentDirectory}\\{dirname}";
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            newpath = Path.Combine(path, $"{i}.txt");
+            using (FileStream fs = File.Create(newpath))
+            { }
         }
     }
 }
